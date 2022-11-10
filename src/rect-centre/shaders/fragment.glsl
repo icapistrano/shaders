44a97f4e2cf738a93f47725a248c6201e7d5c8c8
Immanuel Capistrano <icapistrano97@outlook.com>
2022-11-10 16:15:38 +0000

    2 squares on page

--- src/rect-centre/shaders/fragment.glsl
+++ src/rect-centre/shaders/fragment.glsl
@@ -20,15 +20,17 @@ float rect(vec2 pt, vec2 size, vec2 centre) {
   vec2 halfsize = size / 2.0;
 
   float horz = step(-halfsize.x, offsetpt.x) - step(halfsize.x, offsetpt.x);
-  float vert = step(-halfsize.y, offsetpt.y) - step(halfsize.x, offsetpt.y);
+  float vert = step(-halfsize.y, offsetpt.y) - step(halfsize.y, offsetpt.y);
   
   return horz * vert;
 }
 
+
 void main() {
-  vec3 colour = vec3(0.0);
-  colour.r = rect(vec2(v_position.xy), vec2(0.5), vec2(0.0));
-  colour.g = rect(vec2(v_position.xy), vec2(0.5), vec2(0.0));
+  float square1 = rect(vec2(v_position.xy), vec2(0.3), vec2(-0.5, 0.0));
+  float square2 = rect(vec2(v_position.xy), vec2(0.4), vec2(0.5, 0.0));
+
+  vec3 colour = vec3(1.0, 1.0, 0.0) * square1 + vec3(0.0, 1.0, 0.0) * square2;
 
   gl_FragColor = vec4(colour, 1.0);
 }
